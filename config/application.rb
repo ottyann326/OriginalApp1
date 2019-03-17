c2ade9bdcdb85f6b24069d9134a6da3d6f90aa8f
ottyann326 <ottyann326@gmail.com>
2019-03-18 00:32:56 +0900

    add_config_assets_revenge

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,6 @@ module OriginalApp1
   class Application < Rails::Application
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false
   end
 end
