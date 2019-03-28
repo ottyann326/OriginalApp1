8d92f56b5a09c4e26df0a382acabbdc7e5d2a1d6
ottyann326 <ottyann326@gmail.com>
2019-03-28 22:34:21 +0900

    add validates for racket new_html

--- app/models/racket.rb
+++ app/models/racket.rb
@@ -6,6 +6,9 @@ class Racket < ApplicationRecord
 
   attr_accessor :search_name, :search_price, :search_kind, :search_image
 
+  validates :name, :kind, presence: :true, length: { maximum: 20 }
+  validates :price, numericality: { only_integer: true }
+
   def search
     Racket.ransack(name_cont: @search_name, kind_cont: @search_kind).result
   end
